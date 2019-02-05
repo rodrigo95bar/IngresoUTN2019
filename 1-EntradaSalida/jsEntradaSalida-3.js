fda39e8f8b133181838dddb7892e65bd829bafe0
rodrigo95bar <47338073+rodrigo95bar@users.noreply.github.com>
2019-02-04 22:04:08 -0300

    Add files via upload
    Ejercicios del 1 al 5 realizados

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,14 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostrar()
 {
-	
+	var nombre;
+    var mensaje;
+
+    mensaje = "Tu nombre es";
+
+  nombre =  document.getElementById("elNombre").value;
+
+  alert(mensaje + " " + nombre);
 }
 
 
